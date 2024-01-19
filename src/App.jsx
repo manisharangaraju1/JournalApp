1df4b95620bec4ce65c89a9947f8288ea73d3e35
Manisha Rangaraju <manisharangaraju@Manishas-MacBook-Air.local>
2024-01-18 18:58:18 -0800

    Initial App Setup with webpack and babel

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    const greeting = 'Hello Function Component!';
+    return (<h1>{greeting}</h1>)
+}
+
+export default App;
